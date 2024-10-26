8d88272320667fea0b18ada105b97318c8c1ef4d
Melina Alonso <alonsomelinaluciana@gmail.com>
2024-10-26 10:40:44 -0300

    Cambios de estado en el flores2.gd

--- Scenes/SceneBackground/esceneObjects/flores2.gd
+++ Scenes/SceneBackground/esceneObjects/flores2.gd
@@ -1,19 +1,15 @@
 extends AnimatedSprite2D
 
 @onready var flores_2 = $"."
+var status : bool = false
 
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_area_2d_area_entered(area):
 	if area.name == "CatArea2D":
+		verificar_status_animacion()
+
+func verificar_status_animacion():
+	if status == false:
 		flores_2.play("Romper")
+		status = true
