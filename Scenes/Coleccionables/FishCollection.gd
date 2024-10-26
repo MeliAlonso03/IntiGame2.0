48bbbab0e2685b634bcba2a6e0200536efd0efd7
Melina Alonso <alonsomelinaluciana@gmail.com>
2024-10-26 10:23:23 -0300

    Refactorizacion de clase FishCollection.gd, sacamos la dependencia de la clase player

--- Scenes/Coleccionables/FishCollection.gd
+++ Scenes/Coleccionables/FishCollection.gd
@@ -1,7 +1,6 @@
 extends Area2D
 class_name Collection
 
-@onready var player = $"../CharacterBody2D"
 @onready var sonido_agarrar_fish = $SonidoAgarrarFish
 
 
@@ -16,12 +15,9 @@ func _on_area_entered(area):
 	if area.name == "CatArea2D":
 		sonido_agarrar_fish.play()
 		sonido_agarrar_fish.connect("finished", Callable(self, "_on_sound_finished"))  # Conectar la señal
-		_action_with_cat()
-		
-		
+		area.get_parent().point_suma()
 
-func _action_with_cat():
-	player.point_suma()  # Llama a point_suma() en el Player existente
+
 
 func _on_sound_finished():
 	queue_free()  # Liberar el nodo solo después de que el sonido haya terminado
